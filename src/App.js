efe787e3515668026428c9abb7cb61f7e3e1840f
Hassaan Uddin Sheikh <unbundleracers@gmail.com>
2021-10-11 07:01:33 +0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/navbar/NavBar";
+import NavigationBar from "./components/navbar/NavBar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Pages/home";
 import About from "./components/Pages/about";
 import LogIn from "./components/Pages/logIn";
 import ContactUs from "./components/Pages/contact";
 import PsoIso from "./components/Pages/pos-iso";
+
 function App() {
   return (
     <Router>
-      <Navbar />
+      <NavigationBar />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/About" component={About} />
         <Route path="/PSO-ISO-Program" component={PsoIso} />
-        <Route path="/ContactUs" component={ContactUs} />
+        <Route path="/Contact" component={ContactUs} />
         <Route path="/LogIn" component={LogIn} />
       </Switch>
     </Router>
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,41 +1,36 @@
 import React from "react";
-import {
-  Nav,
-  NavLink,
-  Bars,
-  NavMenu,
-  NavBtn,
-  NavBtnLink,
-} from "./NavbarElements";
 import logo from "../../Assets/Logo/logo-817x824.png";
-
-const Navbar = () => {
+import logo1 from "../../Assets/Logo/logo.png";
+import "../../bootswatch/bootstrap.min.css";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+const NavigationBar = () => {
   return (
     <div>
-      <Nav>
-        <NavLink to="/">
-          <img src={logo} alt="logo" className="logo" />
-        </NavLink>
-        <Bars />
-        <NavMenu>
-          <NavLink to="/About" activeStyle>
-            About
-          </NavLink>
-          <NavLink to="/POS-ISO-Program" activeStyle>
-            Services
-          </NavLink>
-          <NavLink to="/ContactUs" activeStyle>
-            Contact Us
-          </NavLink>
-          {/* Second Nav */}
-          {/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-        </NavMenu>
-        <NavBtn>
-          <NavBtnLink to="/LogIn">Sign In</NavBtnLink>
-        </NavBtn>
-      </Nav>
+      <Navbar className="navbar-dark" bg="primary" expand="lg">
+        <Navbar.Brand href="/">
+          <img src={logo} alt="" width="50" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/About">About</Nav.Link>
+            <Nav.Link href="/Contact">Contact</Nav.Link>
+            <NavDropdown title="Services" id="basic-nav-dropdown" hidden>
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Another action
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
     </div>
   );
 };
-
-export default Navbar;
+export default NavigationBar;
