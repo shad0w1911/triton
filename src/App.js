b057be85abfc41c9e71ba24532deb5e5d06745a9
Hassaan Uddin Sheikh <unbundleracers@gmail.com>
2022-01-18 01:38:30 +0500

    nuked

--- src/App.js
+++ src/App.js
@@ -4,9 +4,7 @@ import NavigationBar from "./components/navbar/NavBar";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Pages/home";
 import About from "./components/Pages/about";
-import LogIn from "./components/Pages/logIn";
 import { Contact } from "./components/Pages/contact";
-import PsoIso from "./components/Pages/pos-iso";
 import Footer from "./components/navbar/footer";
 
 function App() {
@@ -16,9 +14,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/About" component={About} />
-        <Route path="/PSO-ISO-Program" component={PsoIso} />
         <Route path="/Contact" component={Contact} />
-        <Route path="/LogIn" component={LogIn} />
       </Switch>
       <Footer />
     </Router>
